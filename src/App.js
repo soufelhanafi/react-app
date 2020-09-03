d30399f9cf59a903d9f6f850e32dc611d03199a2
Soufian Elhanafi <soufelhanafi@gmail.com>
2020-09-03 19:06:29 +0100

    add switch to make the redirect when path not exist

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TopBar from "./components/topBar"
-import { Route, BrowserRouter, Redirect } from 'react-router-dom';
+import { Route, BrowserRouter, Redirect,Switch } from 'react-router-dom';
 import HomePage from "./pages/homePage"
 import TypesPage from "./pages/typesPage"
 import ViewPokemonPage from "./pages/viewPokemonPage"
@@ -9,11 +9,13 @@ import './App.css';
 function Routes() {
   return(
     <BrowserRouter>
-      <Route exact={true} path="/" component={HomePage} />
-      <Route exact={true} path="/types" component={TypesPage} />
-      <Route path="/view-pokemon/:id" component={ViewPokemonPage} />
-      {/*if the route not exit we make an auto redirect to the home page*/}
-      <Route render={() => <Redirect to="/" />} />
+      <Switch>
+        <Route exact={true} path="/" component={HomePage} />
+        <Route exact={true} path="/types" component={TypesPage} />
+        <Route exact={true} path="/view-pokemon/:id" component={ViewPokemonPage} />
+        {/*if the route not exit or when we make a reload we make an auto redirect to the home page*/}
+        <Route render={() => <Redirect to="/" />} />
+      </Switch>
     </BrowserRouter>
   )
 }
--- src/pages/typesPage/index.js
+++ src/pages/typesPage/index.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default class TypesPage extends React.Component{
   render(){
-    return <h3>Hello Home</h3>
+    return <h3>Hello Types</h3>
   }
 }
